0cb6becfb50ec9f3731a5e103df6dfe087ac3ec8
RobCM <robert.coleman.martinez@gmail.com>
2018-05-16 21:50:49 -0500

    Add files via upload

---
+++ random-quote-machine/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() 
+{  
+  $('#banner').fadeOut(0.1).fadeIn(3000);       
+  $('#body').fadeOut(0.1).delay(1000).fadeIn(3000); 
+  $('#datedone').fadeOut(0.1).delay(1000).fadeIn(3000); 
+});

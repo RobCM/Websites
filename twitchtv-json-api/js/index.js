2c4769ed7abfe89ef508d251d0bd1732485b971f
RobCM <robert.coleman.martinez@gmail.com>
2019-02-03 11:23:47 -0600

    Update index.js

--- twitchtv-json-api/js/index.js
+++ twitchtv-json-api/js/index.js
@@ -8,7 +8,7 @@ function userOnline(channelName)
 {
     //$.getJSON("https://wind-bow.glitch.me/twitch-api/channels/"+channelName+"", function(json){
     $.getJSON("https://api.twitch.tv/kraken/streams?channel="+channelName+"&client_id=e35gnrxztsw5nocsje75p93hlq4hkkn", function(json){ 
-    $("#output").prepend("<div id=cell class='row well'><div class='col'><a target=# href="+json.url+"><img id=userImage height=100px width=100px  src="+json.logo+"></a></div><div class='col'>"+"<a target=# id=username href="+json.url+">"+json.display_name+" </br>" + json.game+ "</a>"+" </br>" +"<h7>"+json.status+"</h7>" +"</div><div class='col'><img id=online height=55px width=55px  src=https://cdn0.iconfinder.com/data/icons/connection/128/icon-19.png></div></div>"); 
+    $("#output").prepend("<div id=cell class='row well'><div class='col'><a target=# href="json.url"><img id=userImage height=100px width=100px  src="json.logo"></a></div><div class='col'>"+"<a target=# id=username href="json.url">"json.display_name" </br>"json.game"</a>"+" </br>" +"<h7>"json.status"</h7>" +"</div><div class='col'><img id=online height=55px width=55px  src=https://cdn0.iconfinder.com/data/icons/connection/128/icon-19.png></div></div>"); 
     });
   }
 /* Function for who is offline */  
@@ -16,7 +16,7 @@ function userOffline(channelName)
 {
     $.getJSON("https://api.twitch.tv/kraken/streams?channel="+channelName+"&client_id=e35gnrxztsw5nocsje75p93hlq4hkkn", function(json){
      
-    $("#output").prepend("<div id=cell class='row well'><div class='col'><img id=userImage height=100px width=100px  src="+json.logo+"></div><div class='col'>"+"<a target=# id=username href="+json.url+">"+json.display_name+"</a>"+"</div><div class='col'><img id=offline height=55px width=55px  src=https://cdn0.iconfinder.com/data/icons/connection/128/icon-12.png></div></div>");  
+    $("#output").prepend("<div id=cell class='row well'><div class='col'><img id=userImage height=100px width=100px  src="json.logo"></div><div class='col'>"+"<a target=# id=username href="json.url">"json.display_name"</a>"+"</div><div class='col'><img id=offline height=55px width=55px  src=https://cdn0.iconfinder.com/data/icons/connection/128/icon-12.png></div></div>");  
   });
 }
   

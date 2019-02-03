9cadea7bdd53a4b96046a36555c28e66d6f0d22a
RobCM <robert.coleman.martinez@gmail.com>
2019-02-03 11:36:39 -0600

    Update index.js

--- twitchtv-json-api/js/index.js
+++ twitchtv-json-api/js/index.js
@@ -2,11 +2,10 @@
 $(document).ready(function()
 {
   /* Using an array for users/channels */
-  var channels = ["JoshOG", "nurseos", "SolaryFortnite", "SCGTour", "GirlGamerChas", "ESL_SC2", "OgamingSC2", "freecodecamp"];  
+  var channels = ["JoshOG", "nurseos", "SolaryFortnite", "SCGTour", "GirlGamerChas"];  
 /* Function for who is online */ 
 function userOnline(channelName)
 {
-    //$.getJSON("https://wind-bow.glitch.me/twitch-api/channels/"+channelName+"", function(json){
     $.getJSON("https://api.twitch.tv/kraken/streams?channel="+channelName+"&client_id=e35gnrxztsw5nocsje75p93hlq4hkkn", function(json){ 
     $("#output").prepend("<div id=cell class='row well'><div class='col'><a target=# href="+json.url+"><img id=userImage height=100px width=100px  src="+json.logo+"></a></div><div class='col'>"+"<a target=# id=username href="+json.url+">"+json.display_name+" </br>"+json.game+"</a>"+" </br>" +"<h7>"+json.status+"</h7>" +"</div><div class='col'><img id=online height=55px width=55px  src=https://cdn0.iconfinder.com/data/icons/connection/128/icon-19.png></div></div>"); 
     });
